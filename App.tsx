26e3d9d8efa3fab74d4a1700674de6de2cec244d
abishek-design <abi9042670@gmail.com>
2025-09-05 19:56:42 +0530

    App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/Header.tsx';
+import Header from './components/Header';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import Testimonials from './components/Testimonials';
